2f1d6fab06267b8c0ca5711e915a0708c9997627
Simon Heath <icefoxen@gmail.com>
2018-05-05 20:11:33 -0400

    Refactored specs dispatcher into scene

--- src/scenes/level.rs
+++ src/scenes/level.rs
@@ -1,38 +1,49 @@
 use ggez;
 use ggez::graphics;
 use ggez_goodies::scene;
-//use ggez_goodies::input as ginput;
-use specs::Join;
+use specs::{self, Join};
 use warmy;
 
 use input;
 use components as c;
 use scenes::*;
+use systems::*;
 use resources;
 use world::World;
 
 pub struct LevelScene {
     done: bool,
     kiwi: warmy::Res<resources::Image>,
+    dispatcher: specs::Dispatcher<'static, 'static>,
 }
 
 impl LevelScene {
     pub fn new(ctx: &mut ggez::Context, world: &mut World) -> Self {
+        let done = false;
         let kiwi = world
             .assets
             .get::<_, resources::Image>(&warmy::FSKey::new("/images/kiwi.png"), ctx)
             .unwrap();
+        let dispatcher = Self::register_systems();
         LevelScene {
-            done: false,
-            kiwi: kiwi,
+            done,
+            kiwi,
+            dispatcher
         }
+    }
+
+
+
+    fn register_systems() -> specs::Dispatcher<'static, 'static> {
+        specs::DispatcherBuilder::new()
+            .add(MovementSystem, "sys_movement", &[])
+            .build()
     }
 }
 
 impl scene::Scene<World, input::InputEvent> for LevelScene {
     fn update(&mut self, gameworld: &mut World) -> FSceneSwitch {
-        gameworld
-            .specs_dispatcher
+        self.dispatcher
             .dispatch(&mut gameworld.specs_world.res);
         if self.done {
             scene::SceneSwitch::Pop
--- src/world.rs
+++ src/world.rs
@@ -14,13 +14,11 @@ use std::path;
 
 use input;
 use components::*;
-use systems::*;
 
 pub struct World {
     pub assets: warmy::Store<ggez::Context>,
     pub input: input::InputState,
     pub specs_world: specs::World,
-    pub specs_dispatcher: specs::Dispatcher<'static, 'static>,
 }
 
 impl World {
@@ -29,13 +27,6 @@ impl World {
         self.specs_world.register::<Motion>();
         self.specs_world.register::<Shot>();
         self.specs_world.register::<Player>();
-    }
-
-    fn register_systems(&mut self) {
-        let dispatcher = specs::DispatcherBuilder::new()
-            .add(MovementSystem, "sys_movement", &[])
-            .build();
-        self.specs_dispatcher = dispatcher;
     }
 
     pub fn new(ctx: &mut ggez::Context, resource_dir: Option<path::PathBuf>) -> Self {
@@ -56,21 +47,13 @@ impl World {
 
         let w = specs::World::new();
 
-        // If we want the dispatcher can go in the Scene instead,
-        // so every scene can have its own set of systems.
-        // For now though, we make a dummy dispatcher that will
-        // get replaced when we call `register_systems()`.
-        let dispatcher = specs::DispatcherBuilder::new().build();
-
         let mut the_world = Self {
             assets: store,
             input: ginput::InputState::new(),
             specs_world: w,
-            specs_dispatcher: dispatcher,
         };
 
         the_world.register_components();
-        the_world.register_systems();
 
         // Make a test entity.
         the_world
